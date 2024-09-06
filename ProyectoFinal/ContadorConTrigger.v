669db59ff32ad739fa67418ceb081fd7f855f536
Juan Delgado <79658379+Juan-delgado1@users.noreply.github.com>
2024-09-05 19:13:42 -0500

    Add files via upload

---
+++ ProyectoFinal/ContadorConTrigger.v
@@ -0,0 +1,32 @@
+module ContadorConTrigger(
+    input wire clk,  
+    input wire echo,        
+    output reg trigger       
+);
+
+// Limite 10us = 10000ns = 500 ciclos de reloj
+
+reg [19:0] contador1;        
+parameter limite = 20'd500;     
+
+initial begin
+    contador1 = 0;
+    trigger = 0;
+end
+
+always @(negedge echo ) begin
+    contador1 = 0;
+    
+end
+
+always @(posedge clk) begin
+    if (contador1 < limite) begin
+        contador1 <= contador1 + 1;
+        trigger <= 1;
+    end else begin
+        
+        trigger = 0;
+    end
+end
+
+endmodule
---
+++ ProyectoFinal/ControlLed.v
@@ -0,0 +1,64 @@
+module ControlLed(
+    input clk,
+	input wire echo,
+    input wire [19:0] contador2,  
+    output reg sens_ult,
+	output reg led1 
+     
+);
+
+// distancia = 340 m/s * tiempo / 2
+// 5cm => 295 us = 295000 ns = 14750 ciclos de reloj
+// 1cm => 59 us = 59000 ns = 2950 ciclos de reloj
+
+
+//10us = 10000ns = 500 ciclos de reloj = 10 bits
+reg [9:0] downSens; //Para 10 bits
+
+initial begin
+	sens_ult=0; 
+	downSens=0;
+end
+
+parameter DOWNSENS=10'd500; //Para 500 ciclos de reloj
+
+parameter L=14'd14750; //Para 5cm
+parameter Lm=14'd2950; //Para 1cm
+
+
+
+
+always @(negedge echo) 
+	begin
+
+		if (contador2>Lm && contador2<L)
+			begin
+				sens_ult=1; 
+				led1 = 1;
+			end
+	    else 
+		begin
+			sens_ult=0;
+			led1=0;
+		end
+	end
+
+always @(posedge clk)
+	begin
+		if (sens_ult==1)
+			begin
+				downSens=downSens+1;
+				if (downSens==DOWNSENS)
+					begin
+						sens_ult=0;
+						downSens=0;
+						led1=0;
+					end
+			end
+		else
+			begin
+				downSens=0;
+			end
+	end
+
+endmodule
