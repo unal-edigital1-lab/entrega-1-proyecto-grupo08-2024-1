08476cfe033fdeda1c37eff03dbea67696ad08bf
Juvargasro <84725878+Juvargasro@users.noreply.github.com>
2024-09-07 11:05:18 -0500

    Add files via upload

--- LCD/tb_bucleEspera.v
+++ LCD/tb_bucleEspera.v
@@ -6,6 +6,7 @@ module tb_bucleEspera;
     reg clk;
     reg reset;
     reg ready_i;
+	 reg [3:0]select_figures;
 		
     // Outputs
     wire rs;
@@ -18,7 +19,7 @@ module tb_bucleEspera;
         .clk(clk), 
         .reset(reset), 
         .ready_i(ready_i),
-        //.select_figures(select_figures), 
+        .select_figures(select_figures), 
         .rs(rs), 
         .rw(rw), 
         .enable(enable), 
@@ -33,39 +34,39 @@ module tb_bucleEspera;
         clk = 0;
         reset = 0;
         ready_i = 0;
-        //select_figures = 5'b00011;
+        select_figures = 4'b0011;
 
         // Apply reset
-        //reset = 1;
-        //#10 reset = 0;
-        //#10 reset = 1;
+        reset = 1;
+        #10 reset = 0;
+        #10 reset = 1;
 
         // Wait for system initialization
         #10 ready_i = 1; 
 
         // Test case 1: select_fig1 = 00 (Gato Feliz), select_fig2 = 000 (Energia)
-        //select_figures = 5'b00011; 
-        //#10000;
+        select_figures = 4'b0101; 
+        #10000;
         
         // Test case 2: select_fig1 = 01 (Gato Triste), select_fig2 = 001 (Diversion)
-        //select_figures = 5'b01001; 
-        //#1000000;
+        select_figures = 4'b0011; 
+        #1000000;
         
-        // Test case 3: select_fig1 = 11 (Gato Neutro), select_fig2 = 010 (Alimentacion)
-        //select_figures = 5'b11010; 
-        //#1000000;
+        // Test case 3: select_fig1 = 11 (Gato Neutro), select_fig2 = 010 (Alimentacion)//SOLO NEUTRO
+        select_figures = 4'b1000; 
+        #1000000;
 
         // Test case 4: select_fig1 = 00 (Gato Feliz), select_fig2 = 011 (Salud)
-        //select_figures = 5'b00011; 
-        //#1000000;
+        select_figures = 4'b0100; 
+        #1000000;
 
         // Test case 5: select_fig1 = 01 (Gato Triste), select_fig2 = 100 (Estado Neutro)
-        //select_figures = 5'b01100; 
+        //select_figures = 4'b01100; 
         //#1000000;
         
-        // Test case 6: select_fig1 = 11 (Gato Neutro), select_fig2 = 000 (Energia)
-        //select_figures = 5'b11000; 
-        //#1000000;
+        // Test case 6: select_fig1 = 11 (Gato Triste), select_fig2 = 000 (Alimentacion)
+        select_figures = 4'b0010; 
+        #1000000;
         
 		  //#(10000000000000000000000) $finish;
 		  
