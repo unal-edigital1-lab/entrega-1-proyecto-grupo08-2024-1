fb6609dac9f8513c82f7b93747b74bd0dc7a5712
Juan Delgado <79658379+Juan-delgado1@users.noreply.github.com>
2024-09-09 02:04:42 -0500

    Update top.v

--- ultrasonido/top.v
+++ ultrasonido/top.v
@@ -1,36 +1,31 @@
 module top (
   input clk,
   input echo,
+  input reset,
   output trig,
-  output sens_ult,
   output led1,
-  output wire [19:0] s0
+  output sens_ult
 );
 
-  
-
-
-  
+  wire clk_out;
+  wire [19:0] s0;
   // ContadorConTrigger
   ContadorConTrigger ContadorConTrigger_i0 (
-    .clk( clk ), //in
-    .echo( echo ), //in
-    .trigger( trig ) //out
+    .clk( clk ),
+    .trigger( trig )
   );
   // ContadorConEcho
   ContadorConEcho ContadorConEcho_i1 (
-    .clk( clk ),       //in
-    .echo( echo ),     //in
-    .contador2( s0 )   //out
+    .clk( clk ),
+    .echo( echo ),
+	 .reset(reset),
+	 .clk_out(clk_out),
+    .contador2( s0 )
   );
   // ControlLed
   ControlLed ControlLed_i2 (
-    .clk( clk ),             //in
-    .contador2( s0 ),        //in
-    .echo( echo ),           //in
-    .led1(led1),             //out
-    .sens_ult( sens_ult )    //out
+    .contador2( s0 ),
+    .led1( led1 ),
+    .sens_ult( sens_ult )
   );
-
-  
 endmodule
