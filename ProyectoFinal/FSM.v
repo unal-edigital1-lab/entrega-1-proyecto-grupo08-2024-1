2de4d1a3aca6732bee96960c1b731d20c0a089c1
Juan-delgado1 <judelgadoe@unal.edu.co>
2024-09-09 13:03:25 -0500

    update

--- ProyectoFinal/FSM.v
+++ ProyectoFinal/FSM.v
@@ -184,7 +184,7 @@ module tamagotchi_fsm (
                     nivel_energia <= nivel_energia - 1; // Aumentar nivel Energía
                 end
             end
-            
+
             if (btn_energia && ledsign) begin
                 display_out[1:0] <= 2'b01; // Mostrar Energía
                 if (nivel_energia < 4'b1010 && display_out == 3'b001) begin
@@ -263,17 +263,17 @@ module tamagotchi_fsm (
     function [6:0] get_seg_display;
         input [3:0] level;
         case (level)
-            4'b0000: get_seg_display = 7'b0111111; // 0
-            4'b0001: get_seg_display = 7'b0000110; // 1
-            4'b0010: get_seg_display = 7'b1011011; // 2
-            4'b0011: get_seg_display = 7'b1001111; // 3
-            4'b0100: get_seg_display = 7'b1100110; // 4
-            4'b0101: get_seg_display = 7'b1101101; // 5
-            4'b0110: get_seg_display = 7'b1111101; // 6
-            4'b0111: get_seg_display = 7'b0000111; // 7
-            4'b1000: get_seg_display = 7'b1111111; // 8
-            4'b1001: get_seg_display = 7'b1101111; // 9
-            4'b1010: get_seg_display = 7'b1110111; // A (utilizado para representar 10)
+            4'b0000: get_seg_display = 7'b1000000; // 0
+            4'b0001: get_seg_display = 7'b1111001; // 1
+            4'b0010: get_seg_display = 7'b0100100; // 2
+            4'b0011: get_seg_display = 7'b0110000; // 3
+            4'b0100: get_seg_display = 7'b0011001; // 4
+            4'b0101: get_seg_display = 7'b0010010; // 5
+            4'b0110: get_seg_display = 7'b0000010; // 6
+            4'b0111: get_seg_display = 7'b1111000; // 7
+            4'b1000: get_seg_display = 7'b0000000; // 8
+            4'b1001: get_seg_display = 7'b0010000; // 9
+            //4'b1010: get_seg_display = 7'b1110111; // A (utilizado para representar 10)
             default: get_seg_display = 7'b0000000; // Apagar todos los segmentos
         endcase
     endfunction
