ce2b5247087ab71f6563033290959b95bdda611b
Juan Delgado <79658379+Juan-delgado1@users.noreply.github.com>
2024-09-09 02:01:55 -0500

    Update ContadorConEcho.v

--- ultrasonido/ContadorConEcho.v
+++ ultrasonido/ContadorConEcho.v
@@ -1,14 +1,32 @@
 module ContadorConEcho(
     input wire clk,           // Entrada de reloj
     input wire echo,          // Entrada de control (cuando echo=1, se incrementa el contador)
+	 input wire reset,
+	 output reg clk_out,
     output reg [19:0] contador2 // Salida del contador de 32 bits
 );
 
-initial begin
-	contador2 = 0; // Inicializa el contador
-end
+reg [22:0] counter;      // Suficientemente grande para contar hasta 7,500,000
+    //parameter DIVISOR = 7500000;
+    //parameter DIVISOR = 3750000;
+    parameter DIVISOR = 1;
 
-always @(posedge clk) 
+
+always @(posedge clk or posedge reset) begin
+        if (reset) begin
+            counter <= 0;
+            clk_out <= 0;
+        end else begin
+            if (counter == DIVISOR-1) begin
+                counter <= 0;
+                clk_out <= ~clk_out;  // Invierte la señal para generar el nuevo clk
+            end else begin
+                counter <= counter + 1;
+            end
+        end
+    end
+
+always @(posedge clk_out) 
     begin
 		if(echo==1)
 			begin
